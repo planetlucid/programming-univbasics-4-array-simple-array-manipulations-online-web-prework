362630145020ec0d4839ca53add8e3c3d41d70da
restful-strategy-4685 <planetlucid@gmail.com>
2019-08-02 22:42:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,7 +6,7 @@ def using_unshift(array, string)
   array.unshift(string)
 end
 
-def using_pop(array, string)
+def using_pop(array)
   array.pop
 end
 
@@ -22,8 +22,8 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using_concat(array_1, array_2)
-  array1.concat(array_2)
+def using_concat(array1, array2)
+  array1.concat(array2)
 end
 
 def using_insert(array, item)
